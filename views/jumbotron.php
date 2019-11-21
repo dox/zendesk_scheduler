83185b7f54d84800913812f3e895ce96b7a645f7
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2019-11-21 12:44:39 +0000

    Adding custom zendesk url option to config

--- views/jumbotron.php
+++ views/jumbotron.php
@@ -1,5 +1,5 @@
 <div class="jumbotron">
 	<h1 class="display-4">Zendesk Scheduler</h1>
-	<p class="lead">A utility to allow the automatic/scheduled ticket creation on the St Edmund Hall Zendesk helpdesk.</p>
-	<p><a class="btn btn-lg btn-success" href="https://stedmundhall.zendesk.com" role="button">View Zendesk</a></p>
+	<p class="lead">A utility to allow the automatic creation of scheduled tickets on the Zendesk helpdesk.</p>
+	<p><a class="btn btn-lg btn-success" href="<?php echo zendesk_url; ?>" role="button">View Zendesk</a></p>
 </div>
