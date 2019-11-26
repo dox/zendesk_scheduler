f3868da87b601a97542e908bba6437622719e8e0
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2019-11-26 08:51:08 +0000

    Adding some basic steps

--- index.php
+++ index.php
@@ -9,25 +9,22 @@
 	
 	<div class="row">
 		<div class="col-lg-6">
-			<h4>Subheading</h4>
-			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+			<h4>Step 1:</h4>
+			<p>Make sure you have installed the excellent <a href="https://github.com/zendesk/zendesk_api_client_php">Zendesk API Client</a> into the same directory as the above files using composer</p>
 			
-			<h4>Subheading</h4>
-			<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+			<h4>Step 2</h4>
+			<p>Ensure you have configured cron to run the necessary files (see the README)</p>
 			
-			<h4>Subheading</h4>
-			<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+			<h4>Step 3</h4>
+			<p>Setup your Zendesk <a href="agents.php">agents</a>.</p>
 		</div>
 
 		<div class="col-lg-6">
-			<h4>Subheading</h4>
-			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+			<h4>Step 4</h4>
+			<p>Setup your Zendesk <a href="jobs.php">tasks</a>.</p>
 			
-			<h4>Subheading</h4>
-			<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-			
-			<h4>Subheading</h4>
-			<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+			<h4>Step 6</h4>
+			<p>Sit back and wait for the jobs to come in!</p>
 		</div>
 	</div>
 	
