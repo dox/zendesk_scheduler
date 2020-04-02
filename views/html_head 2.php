720eb06169ecfc9f93cf524be7ae99920f2f60c4
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-04-02 09:33:51 +0100

    UI tweaks

---
+++ views/html_head 2.php
@@ -0,0 +1,28 @@
+<?php
+include_once("inc/database.php");
+include_once("inc/jobs.php");
+include_once("inc/team.php");
+include_once("inc/logs.php");
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    
+    <title>SEH Task Scheduler</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <!--<link href="css/jumbotron-narrow.css" rel="stylesheet">-->
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
---
+++ views/jumbotron 2.php
@@ -0,0 +1,5 @@
+<div class="jumbotron">
+	<h1 class="display-4">Zendesk Scheduler</h1>
+	<p class="lead">A simple web-based utility to create and manage reoccurring Zendesk ticket creation</p>
+	<p><a class="btn btn-lg btn-success" href="<?php echo zendesk_url; ?>" role="button">View Zendesk</a></p>
+</div>
