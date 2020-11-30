a19a66f1eb481e66959583e7ec6869b7d75e7730
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-11-30 17:49:16 +0000

    Typo

--- nodes/index.php
+++ nodes/index.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="px-3 py-3 pt-md-5 pb-md-4 text-center">
-		<h1 class="display-4">Zendesk Schedule</h1>
+		<h1 class="display-4">Zendesk Scheduler</h1>
 		<p class="lead">A simple web-based utility to create and manage reoccurring Zendesk ticket creation.</p>
 	</div>
 
